22f79d61612a3df3ed1d86af02ecc323c6812a63
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-03-03 10:21:03 +0100

    Remove unused StringUtil.java

--- src/main/java/no/nav/syfo/util/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package no.nav.syfo.util;
-
-import org.apache.commons.lang.StringUtils;
-
-public class StringUtil {
-    public static String lowerCapitalize(String input) {
-        return StringUtils.capitalize(input.toLowerCase());
-    }
-}
