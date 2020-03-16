07914ab7bff049a764298149651b2d5c86f49740
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-03-16 10:13:10 +0100

    Remove Veilederoppgaver-func
    Syfoveilederoppgaver does no longer exists and functionality for generating Veilederoppgaver is redudant

--- src/main/java/no/nav/syfo/util/PropertyUtil.java
+++ src/main/java/no/nav/syfo/util/PropertyUtil.java
@@ -9,7 +9,6 @@ public class PropertyUtil {
     public final static String FASIT_ENVIRONMENT_NAME = "FASIT_ENVIRONMENT_NAME";
     public final static String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
 
-    public final static String SYFOVEILEDEROPPGAVER_SYSTEMAPI = "syfoveilederoppgaver.systemapi";
     public final static String FASTLEGE_DIALOGMELDING_API_URL = "FASTLEGE_DIALOGMELDING_API_V1_URL";
 
     public final static String ALTINN_TEST_WHITELIST_ORGNR = "ALTINN_TEST_WHITELIST_ORGNR";
