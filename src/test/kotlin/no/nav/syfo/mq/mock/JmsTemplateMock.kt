8d046f3243b7b6388dc1c9ecb5ff44b30a82abf7
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-06-30 11:39:59 +0200

    Convert mq-test-config to Kotlin

---
+++ src/test/kotlin/no/nav/syfo/mq/mock/JmsTemplateMock.kt
@@ -0,0 +1,19 @@
+package no.nav.syfo.mq.mock
+
+import org.springframework.jms.JmsException
+import org.springframework.jms.UncategorizedJmsException
+import org.springframework.jms.core.JmsTemplate
+import org.springframework.jms.core.MessageCreator
+import javax.jms.JMSException
+import javax.jms.TextMessage
+
+class JmsTemplateMock(private val name: String) : JmsTemplate(ConnectionFactoryMock()) {
+    @Throws(JmsException::class)
+    override fun send(messageCreator: MessageCreator) {
+        try {
+            val message = messageCreator.createMessage(SessionMock()) as TextMessage
+        } catch (e: JMSException) {
+            throw UncategorizedJmsException(e)
+        }
+    }
+}
