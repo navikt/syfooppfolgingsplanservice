bd83e87ac74b9269ea76b7248e4d3ad45f91702e
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-06-30 13:36:10 +0200

    Convert Toggle to Kotlin

--- src/main/kotlin/no/nav/syfo/toggle/Toggle.kt
+++ src/main/kotlin/no/nav/syfo/toggle/Toggle.kt
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Component
 
 @Component
 class Toggle(
-    @Value("\${toggle.enable.batch:false}") private val toggleBatch: Boolean,
-    @Value("\${toggle.enable.batch.sak:true}") private val toggleBatchSak: Boolean,
     @Value("\${nais.cluster.name:prod-fss}") private val envName: String
 ) {
     fun erPreprod(): Boolean {
