5dc470fab03a02a2a68262998811a85d05137c98
Sindre Broch <sindrebroch@Sindres-MBP.localdomain>
2020-09-14 10:21:56 +0200

    Fixed variables in create table

--- src/main/resources/db/migration/V1_30__add_feilet_sending.sql
+++ src/main/resources/db/migration/V1_30__add_feilet_sending.sql
@@ -10,8 +10,8 @@ CREATE SEQUENCE FEILET_SENDING_ID_SEQ START WITH 1 INCREMENT BY 1;
 CREATE TABLE FEILET_SENDING (
   id                          NUMBER(19, 0) NOT NULL,
   oppfolgingsplanlps_id       NUMBER(19, 0) NOT NULL,
-  number_of_tries             NUMBER(2) NOT NULL,
-  max_retries                 NUMBER(2) NOT NULL,
+  number_of_tries             NUMBER(4, 0) NOT NULL,
+  max_retries                 NUMBER(4, 0) NOT NULL,
   opprettet                   TIMESTAMP NOT NULL,
   sist_endret                 TIMESTAMP NOT NULL,
   CONSTRAINT FEILET_SENDING_PK PRIMARY KEY (id),
