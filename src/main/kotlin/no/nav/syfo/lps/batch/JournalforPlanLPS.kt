2698f277d4894445d90b2c7e1c7f00712af4a20c
Sindre Broch <sindrebroch@Sindres-MBP.localdomain>
2020-09-16 13:47:54 +0200

    Endret til å prøve på nytt en gang i timen

--- src/main/kotlin/no/nav/syfo/lps/batch/JournalforPlanLPS.kt
+++ src/main/kotlin/no/nav/syfo/lps/batch/JournalforPlanLPS.kt
@@ -16,7 +16,7 @@ class JournalforPlanLPS @Inject constructor(
 ) {
     private val isDev = naisClusterName == "dev-fss"
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 60 * 60 * 1000)
     fun createOppfolgingsplanLPSJournalposter() {
         if (isDev && leaderElectionService.isLeader && "true" != System.getProperty(PropertyUtil.LOCAL_MOCK)) {
             oppfolgingsplanLPSService.createOppfolgingsplanLPSJournalposter()
