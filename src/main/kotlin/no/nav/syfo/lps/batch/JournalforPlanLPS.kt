508cc072bd436e790dc735da32930ea314356e63
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2020-10-02 12:08:24 +0200

    Send alle planer inn i eget løp (#134)

--- src/main/kotlin/no/nav/syfo/lps/batch/JournalforPlanLPS.kt
+++ src/main/kotlin/no/nav/syfo/lps/batch/JournalforPlanLPS.kt
@@ -3,18 +3,15 @@ package no.nav.syfo.lps.batch
 import no.nav.syfo.lps.OppfolgingsplanLPSService
 import no.nav.syfo.service.LeaderElectionService
 import no.nav.syfo.util.PropertyUtil
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import javax.inject.Inject
 
 @Service
 class JournalforPlanLPS @Inject constructor(
-    @Value("\${nais.cluster.name}") private val naisClusterName: String,
     private val oppfolgingsplanLPSService: OppfolgingsplanLPSService,
     private val leaderElectionService: LeaderElectionService
 ) {
-    private val isDev = naisClusterName == "dev-fss"
 
     @Scheduled(fixedRate = 60 * 10 * 1000)
     fun createOppfolgingsplanLPSJournalposter() {
