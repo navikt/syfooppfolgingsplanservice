ebaeef74995157f8dade4fc77181eb14635e88ee
Tor-Christian Tangenes <torchrct@gmail.com>
2020-10-05 10:40:25 +0200

    Fjerner dev-toggle og legger på leader sjekk på 'fastlege deling feilet'-jobb

--- src/main/kotlin/no/nav/syfo/lps/batch/OppfolgingsplanLPS.kt
+++ src/main/kotlin/no/nav/syfo/lps/batch/OppfolgingsplanLPS.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.lps.batch
 import no.nav.syfo.domain.FeiletSending
 import no.nav.syfo.lps.OppfolgingsplanLPSService
 import no.nav.syfo.repository.dao.FeiletSendingDAO
+import no.nav.syfo.service.LeaderElectionService
 import no.nav.syfo.util.PropertyUtil
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
@@ -11,15 +12,14 @@ import javax.inject.Inject
 
 @Service
 class OppfolgingsplanLPS @Inject constructor(
-    @Value("\${nais.cluster.name}") private val naisClusterName: String,
+    private val leaderElectionService: LeaderElectionService,
     private val oppfolgingsplanLPSService: OppfolgingsplanLPSService,
     private val feiletSendingDAO: FeiletSendingDAO
 ) {
-    private val isDev = naisClusterName == "dev-fss"
 
-    @Scheduled(fixedDelay = 60000)
+    @Scheduled(fixedDelay = 60 * 1000)
     fun retrySendOppfolgingsplanLpsTilFastlege() {
-        if (isDev && "true" != System.getProperty(PropertyUtil.LOCAL_MOCK)) {
+        if (leaderElectionService.isLeader && "true" != System.getProperty(PropertyUtil.LOCAL_MOCK)) {
             val liste: List<FeiletSending> = feiletSendingDAO.hentFeiledeSendinger()
 
             for(feiletSending in liste) {
