6281eb30eef73e500d8a443e9ce976bef274c135
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-13 21:12:04 +0200

    QTTD2aqq: Slettet unødvendige DTO klasser

--- src/main/java/no/nav/syfo/sykmeldinger/dto/ArbeidsgiverStatusDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/ArbeidsgiverStatusDTO.java
@@ -1,10 +1,12 @@
 package no.nav.syfo.sykmeldinger.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(fluent = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ArbeidsgiverStatusDTO {
     public String orgnummer;
     public String juridiskOrgnummer;
