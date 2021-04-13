8e3756b5523bb45d9bb1870d3dc149d205864b6e
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-13 21:16:02 +0200

    QTTD2aqq: Slettet unødvendige DTO klasser

--- src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingDTO.java
@@ -3,41 +3,17 @@ package no.nav.syfo.sykmeldinger.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.experimental.Accessors;
-import org.joda.time.LocalDate;
 
-import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Map;
 
 @Data
 @Accessors(fluent = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SykmeldingDTO {
     public String id;
-    public OffsetDateTime mottattTidspunkt;
     public BehandlingsutfallDTO behandlingsutfall;
-    public String legekontorOrgnummer;
-    public ArbeidsgiverDTO arbeidsgiver;
     public List<SykmeldingsperiodeDTO> sykmeldingsperioder;
     public SykmeldingStatusDTO sykmeldingStatus;
-    public MedisinskVurderingDTO medisinskVurdering;
-    public boolean skjermesForPasient;
-    public PrognoseDTO prognose;
-    public Map<String, Map<String, SporsmalSvarDTO>> utdypendeOpplysninger;
-    public String tiltakArbeidsplassen;
-    public String tiltakNAV;
-    public String andreTiltak;
-    public MeldingTilNavDTO meldingTilNAV;
-    public String meldingTilArbeidsgiver;
-    public KontaktMedPasientDTO kontaktMedPasient;
-    public OffsetDateTime behandletTidspunkt;
-    public BehandlerDTO behandler;
-    public LocalDate syketilfelleStartDato;
-    public String navnFastlege;
-    public boolean egenmeldt;
-    public boolean papirsykmelding;
-    public boolean harRedusertArbeidsgiverperiode;
-    public List<MerknadDTO> merknader;
 }
 
 
--- src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingStatusDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingStatusDTO.java
@@ -1,17 +1,16 @@
 package no.nav.syfo.sykmeldinger.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 
 @Data
 @Accessors(fluent = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SykmeldingStatusDTO {
     public String statusEvent;
     public OffsetDateTime timestamp;
     public ArbeidsgiverStatusDTO arbeidsgiver;
-    List<SporsmalDTO> sporsmalOgSvarListe;
-
 }
