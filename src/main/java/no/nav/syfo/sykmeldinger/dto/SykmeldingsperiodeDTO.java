7fccf33972fda133f0338b111f986e8a94d53431
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-23 05:38:41 +0200

    QTTD2aqq: Lagt til @JsonDeserialize(using = LocalDateTimeDeserializer.class) og @JsonSerialize(using = LocalDateSerializer.class)

--- src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingsperiodeDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingsperiodeDTO.java
@@ -1,6 +1,9 @@
 package no.nav.syfo.sykmeldinger.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.joda.time.LocalDate;
@@ -8,6 +11,10 @@ import org.joda.time.LocalDate;
 @Data
 @Accessors(fluent = true)
 public class SykmeldingsperiodeDTO {
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     public LocalDate fom;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     public LocalDate tom;
 }
