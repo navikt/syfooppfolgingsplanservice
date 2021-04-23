c9371559f08b1d17ccfdcd914745dc03a130f6f4
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-23 09:37:32 +0200

    QTTD2aqq: implement Serializable på felter

--- src/main/java/no/nav/syfo/model/OrganisasjonsInformasjon.java
+++ src/main/java/no/nav/syfo/model/OrganisasjonsInformasjon.java
@@ -3,9 +3,11 @@ package no.nav.syfo.model;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 @Data
 @Accessors(fluent = true)
-public class OrganisasjonsInformasjon {
+public class OrganisasjonsInformasjon implements Serializable {
     public String orgnummer;
     public String orgNavn;
 }
--- src/main/java/no/nav/syfo/model/Sykmeldingsperiode.java
+++ src/main/java/no/nav/syfo/model/Sykmeldingsperiode.java
@@ -3,11 +3,12 @@ package no.nav.syfo.model;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Accessors(fluent = true)
-public class Sykmeldingsperiode {
+public class Sykmeldingsperiode implements Serializable {
     public LocalDate fom;
     public LocalDate tom;
 }
