3cfe903ad1144f9f30ee69d4a8be6d5613749f70
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-23 07:29:11 +0200

    QTTD2aqq: Lagt til implements Serializable (*facepalm*)

--- src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingDTO.java
@@ -4,12 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Accessors(fluent = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SykmeldingDTO {
+public class SykmeldingDTO implements Serializable {
     public String id;
     public BehandlingsutfallDTO behandlingsutfall;
     public List<SykmeldingsperiodeDTO> sykmeldingsperioder;
