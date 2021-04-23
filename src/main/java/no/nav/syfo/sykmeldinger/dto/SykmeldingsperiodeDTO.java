9639a1c80f15d6444d53531d8a7abaddff2f90bd
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-23 07:59:02 +0200

    QTTD2aqq: Endret typer til datoer

--- src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingsperiodeDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingsperiodeDTO.java
@@ -9,6 +9,6 @@ import java.time.LocalDateTime;
 @Data
 @Accessors(fluent = true)
 public class SykmeldingsperiodeDTO {
-    public LocalDateTime fom;
-    public LocalDateTime tom;
+    public String fom;
+    public String tom;
 }
