567f03d73ed3ba26656c9ed69b951f3f8c2093fb
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-26 08:49:53 +0200

    QTTD2aqq: Fjernet SykmeldingerConsumer depandency fra NL controller

--- src/main/java/no/nav/syfo/api/selvbetjening/controller/NarmesteLederController.java
+++ src/main/java/no/nav/syfo/api/selvbetjening/controller/NarmesteLederController.java
@@ -2,31 +2,31 @@ package no.nav.syfo.api.selvbetjening.controller;
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import no.nav.security.token.support.core.context.TokenValidationContextHolder;
-
-import no.nav.syfo.aktorregister.AktorregisterConsumer;
-import no.nav.syfo.metric.Metrikk;
-import no.nav.syfo.model.Naermesteleder;
-import no.nav.syfo.model.Sykmelding;
-import no.nav.syfo.narmesteleder.NarmesteLederConsumer;
-import no.nav.syfo.service.BrukertilgangService;
-import no.nav.syfo.sykmeldinger.SykmeldingerConsumer;
-
 import org.slf4j.Logger;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
-import java.util.List;
 import java.util.Optional;
 
 import static no.nav.syfo.oidc.OIDCIssuer.EKSTERN;
 import static no.nav.syfo.oidc.OIDCUtil.getSubjectEksternMedThrows;
 import static no.nav.syfo.util.RequestUtilKt.NAV_PERSONIDENT_HEADER;
 import static org.slf4j.LoggerFactory.getLogger;
+
+import no.nav.syfo.aktorregister.AktorregisterConsumer;
+import no.nav.syfo.metric.Metrikk;
+import no.nav.syfo.model.Naermesteleder;
+import no.nav.syfo.narmesteleder.NarmesteLederConsumer;
+import no.nav.syfo.service.BrukertilgangService;
 
 @RestController
 @ProtectedWithClaims(issuer = EKSTERN)
@@ -40,7 +40,6 @@ public class NarmesteLederController {
     private final AktorregisterConsumer aktorregisterConsumer;
     private final BrukertilgangService brukertilgangService;
     private final NarmesteLederConsumer narmesteLederConsumer;
-    private final SykmeldingerConsumer sykmeldingerConsumer;
 
     @Inject
     public NarmesteLederController(
@@ -48,15 +47,13 @@ public class NarmesteLederController {
             Metrikk metrikk,
             AktorregisterConsumer aktorregisterConsumer,
             BrukertilgangService brukertilgangService,
-            NarmesteLederConsumer narmesteLederConsumer,
-            SykmeldingerConsumer sykmeldingerConsumer
+            NarmesteLederConsumer narmesteLederConsumer
     ) {
         this.oidcContextHolder = oidcContextHolder;
         this.metrikk = metrikk;
         this.aktorregisterConsumer = aktorregisterConsumer;
         this.brukertilgangService = brukertilgangService;
         this.narmesteLederConsumer = narmesteLederConsumer;
-        this.sykmeldingerConsumer = sykmeldingerConsumer;
     }
 
     @ResponseBody
@@ -83,7 +80,6 @@ public class NarmesteLederController {
             String oppslattIdentAktorId = aktorregisterConsumer.hentAktorIdForFnr(oppslaattIdent);
 
             Optional<Naermesteleder> narmesteLeder = narmesteLederConsumer.narmesteLeder(oppslattIdentAktorId, virksomhetsnummer);
-
             if (narmesteLeder.isPresent()) {
                 return ResponseEntity
                         .status(HttpStatus.OK)
