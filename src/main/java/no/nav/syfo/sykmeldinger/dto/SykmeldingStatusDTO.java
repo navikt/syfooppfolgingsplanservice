337f23392491d7f0e630ab4c2e64c981c65b75c8
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-26 08:35:40 +0200

    QTTD2aqq: Slettet ignors

--- src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingStatusDTO.java
+++ src/main/java/no/nav/syfo/sykmeldinger/dto/SykmeldingStatusDTO.java
@@ -1,6 +1,5 @@
 package no.nav.syfo.sykmeldinger.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -9,7 +8,6 @@ import java.time.OffsetDateTime;
 
 @Data
 @Accessors(fluent = true)
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class SykmeldingStatusDTO implements Serializable {
     public String statusEvent;
     public OffsetDateTime timestamp;
