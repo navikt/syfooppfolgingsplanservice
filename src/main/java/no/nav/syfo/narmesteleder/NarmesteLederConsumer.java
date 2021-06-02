9e6262095b9ad98199f8f4bb2b89af8a65edd5a7
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-06-02 11:46:50 +0200

    92nVNthe: La til debug logging for å kunne feilsøke. Skal reverteres.

--- src/main/java/no/nav/syfo/narmesteleder/NarmesteLederConsumer.java
+++ src/main/java/no/nav/syfo/narmesteleder/NarmesteLederConsumer.java
@@ -104,6 +104,7 @@ public class NarmesteLederConsumer {
         throwExceptionIfError(response.getStatusCode(), HENT_ANSATTE_SYFONARMESTELEDER_FEILET);
 
         metrikk.tellHendelse(HENT_ANSATTE_SYFONARMESTELEDER_VELLYKKET);
+        LOG.info("Response fra ny narmeste leder-app: " + response.getBody());
         return mapListe(response.getBody(), narmestelederRelasjon2Ansatt);
     }
 
