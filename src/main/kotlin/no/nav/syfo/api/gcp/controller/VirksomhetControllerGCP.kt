2818746f7bd4df4a52c685c89bd8410c5f9cc907
Tor-Christian Tangenes <torchrct@gmail.com>
2022-03-14 19:23:54 +0100

    Legg til virksomhetscontroller

---
+++ src/main/kotlin/no/nav/syfo/api/gcp/controller/VirksomhetControllerGCP.kt
@@ -0,0 +1,34 @@
+package no.nav.syfo.api.gcp.controller
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.syfo.api.gcp.domain.VirksomhetGCP
+import no.nav.syfo.ereg.EregConsumer
+import no.nav.syfo.oidc.OIDCIssuer
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.inject.Inject
+
+
+@RestController
+@ProtectedWithClaims(issuer = OIDCIssuer.EKSTERN)
+@RequestMapping(value = ["/api/gcp/virksomhet/{virksomhetsnummer}"])
+class VirksomhetControllerGCP @Inject constructor(
+    private val eregConsumer: EregConsumer,
+) {
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getVirksomhet(
+        @PathVariable("virksomhetsnummer") virksomhetsnummer: String
+    ): ResponseEntity<VirksomhetGCP> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(VirksomhetGCP(
+                virksomhetsnummer = virksomhetsnummer,
+                navn = eregConsumer.virksomhetsnavn(virksomhetsnummer)
+            ))
+    }
+}
---
+++ src/main/kotlin/no/nav/syfo/api/gcp/domain/VirksomhetGCP.kt
@@ -0,0 +1,6 @@
+package no.nav.syfo.api.gcp.domain
+
+data class VirksomhetGCP(
+    val virksomhetsnummer: String,
+    val navn: String = ""
+)
