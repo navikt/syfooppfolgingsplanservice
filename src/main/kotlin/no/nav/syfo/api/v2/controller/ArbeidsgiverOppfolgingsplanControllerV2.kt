fbd3ad0eaa8910a82c66fb280870129fbe7fefa0
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-09-12 15:39:50 +0200

    Revert "Test"
    This reverts commit 88ac0c1a1db74e626b1d2dad4b79d6146f159e95.

--- src/main/kotlin/no/nav/syfo/api/v2/controller/ArbeidsgiverOppfolgingsplanControllerV2.kt
+++ src/main/kotlin/no/nav/syfo/api/v2/controller/ArbeidsgiverOppfolgingsplanControllerV2.kt
@@ -35,7 +35,7 @@ class ArbeidsgiverOppfolgingsplanControllerV2 @Inject constructor(
 ) {
     @GetMapping(produces = [APPLICATION_JSON_VALUE])
     fun hentArbeidsgiversOppfolgingsplaner(): List<RSBrukerOppfolgingsplan> {
-        val innloggetIdent = TokenXUtil.validateTokenXClaims(contextHolder, tokenxIdp, oppfolgingsplanClientId, "dev-gcp:plattformsikkerhet:debug-dings")
+        val innloggetIdent = TokenXUtil.validateTokenXClaims(contextHolder, tokenxIdp, oppfolgingsplanClientId)
             .fnrFromIdportenTokenX()
             .value
         val liste = mapListe(
