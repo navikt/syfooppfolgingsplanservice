5c22dd6694b78c3d73a357e6ce9c392a12f5b580
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-09-21 12:37:18 +0200

    Add controller and test for kontaktinfo

---
+++ src/main/kotlin/no/nav/syfo/api/v3/controller/KontaktinfoControllerV3.kt
@@ -0,0 +1,75 @@
+package no.nav.syfo.api.v3.controller
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.syfo.api.v2.domain.Kontaktinfo
+import no.nav.syfo.api.v2.util.fodselsnummerInvalid
+import no.nav.syfo.dkif.DigitalKontaktinfo
+import no.nav.syfo.dkif.DkifConsumer
+import no.nav.syfo.service.BrukertilgangService
+import no.nav.syfo.tokenx.TokenXUtil
+import no.nav.syfo.tokenx.TokenXUtil.TokenXIssuer.TOKENX
+import no.nav.syfo.tokenx.TokenXUtil.fnrFromIdportenTokenX
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.inject.Inject
+
+
+@RestController
+@ProtectedWithClaims(issuer = TOKENX, claimMap = ["acr=Level4"])
+@RequestMapping(value = ["/api/v3/kontaktinfo/{fnr}"])
+class KontaktinfoControllerV3 @Inject constructor(
+    private val contextHolder: TokenValidationContextHolder,
+    private val brukertilgangService: BrukertilgangService,
+    private val dkifConsumer: DkifConsumer,
+    @Value("\${tokenx.idp}")
+    private val tokenxIdp: String,
+    @Value("\${oppfolgingsplan.frontend.client.id}")
+    private val oppfolgingsplanClientId: String,
+) {
+    @ResponseBody
+    @GetMapping(produces = [APPLICATION_JSON_VALUE])
+    fun getKontaktinfo(
+        @PathVariable("fnr") fnr: String
+    ): ResponseEntity<Kontaktinfo> {
+        val innloggetFnr = TokenXUtil.validateTokenXClaims(contextHolder, tokenxIdp, oppfolgingsplanClientId)
+            .fnrFromIdportenTokenX()
+            .value
+
+        return if (fodselsnummerInvalid(fnr)) {
+            LOG.error("Ugyldig fnr ved henting av kontaktinfo")
+            ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .build()
+        } else {
+            if (!brukertilgangService.tilgangTilOppslattIdent(innloggetFnr, fnr)) {
+                LOG.error("Ikke tilgang til kontaktinfo: Bruker spør om noen andre enn seg selv eller egne ansatte")
+                ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .build()
+            } else {
+                val kontaktinfo = dkifConsumer.kontaktinformasjon(fnr)
+                ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(Kontaktinfo(
+                        fnr = fnr,
+                        epost = kontaktinfo.epostadresse,
+                        tlf = kontaktinfo.mobiltelefonnummer,
+                        skalHaVarsel = kanVarsles(kontaktinfo)
+                    ))
+            }
+        }
+
+    }
+
+    private fun kanVarsles(digitalKontaktinfo: DigitalKontaktinfo) =
+        (digitalKontaktinfo.reservert == false) && digitalKontaktinfo.kanVarsles
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(KontaktinfoControllerV3::class.java)
+    }
+}
---
+++ src/test/kotlin/no/nav/syfo/api/v3/controller/KontaktinfoControllerV3Test.kt
@@ -0,0 +1,177 @@
+package no.nav.syfo.api.v3.controller
+
+import no.nav.syfo.api.AbstractRessursTilgangTest
+import no.nav.syfo.api.v2.domain.Kontaktinfo
+import no.nav.syfo.brukertilgang.BrukertilgangConsumer
+import no.nav.syfo.dkif.DigitalKontaktinfo
+import no.nav.syfo.dkif.DkifConsumer
+import no.nav.syfo.sts.StsConsumer
+import no.nav.syfo.testhelper.UserConstants
+import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_FNR
+import no.nav.syfo.testhelper.UserConstants.LEDER_FNR
+import no.nav.syfo.testhelper.generateDigitalKontaktinfo
+import no.nav.syfo.testhelper.loggInnBrukerTokenX
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.ResponseEntity
+import javax.inject.Inject
+
+class KontaktinfoControllerV3Test : AbstractRessursTilgangTest() {
+
+    @MockBean
+    lateinit var brukertilgangConsumer: BrukertilgangConsumer
+
+    @MockBean
+    lateinit var dkifConsumer: DkifConsumer
+
+    @MockBean
+    private lateinit var stsConsumer: StsConsumer
+
+    @Inject
+    private lateinit var kontaktinfoController: KontaktinfoControllerV3
+
+    @Value("\${tokenx.idp}")
+    private lateinit var tokenxIdp: String
+
+    @Value("\${oppfolgingsplan.frontend.client.id}")
+    private lateinit var oppfolgingsplanClientId: String
+
+    @Before
+    fun setup() {
+        `when`(stsConsumer.token()).thenReturn("token")
+
+    }
+
+    @Test
+    fun narmesteLeder_ansatt_ok() {
+        loggInnBrukerTokenX(contextHolder, LEDER_FNR, oppfolgingsplanClientId, tokenxIdp)
+
+        `when`(brukertilgangConsumer.hasAccessToAnsatt(ARBEIDSTAKER_FNR)).thenReturn(true)
+        val digitalKontaktinfo = DigitalKontaktinfo(
+            epostadresse = UserConstants.PERSON_EMAIL,
+            kanVarsles = false,
+            reservert = false,
+            mobiltelefonnummer = UserConstants.PERSON_TLF,
+            personident = ARBEIDSTAKER_FNR
+        )
+        `when`(dkifConsumer.kontaktinformasjon(ArgumentMatchers.anyString())).thenReturn(digitalKontaktinfo)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        val body = res.body as Kontaktinfo
+        assertEquals(200, res.statusCodeValue.toLong())
+        assertEquals(ARBEIDSTAKER_FNR, body.fnr)
+        assertEquals(digitalKontaktinfo.epostadresse, body.epost)
+        assertEquals(digitalKontaktinfo.mobiltelefonnummer, body.tlf)
+        assertNull(body.feilAarsak)
+    }
+
+    @Test
+    fun narmesteLeder_self_ok() {
+        loggInnBrukerTokenX(contextHolder, ARBEIDSTAKER_FNR, oppfolgingsplanClientId, tokenxIdp)
+        val digitalKontaktinfo = generateDigitalKontaktinfo()
+        `when`(dkifConsumer.kontaktinformasjon(ArgumentMatchers.anyString())).thenReturn(digitalKontaktinfo)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        val body = res.body as Kontaktinfo
+        assertEquals(200, res.statusCodeValue.toLong())
+        assertEquals(ARBEIDSTAKER_FNR, body.fnr)
+        assertEquals(digitalKontaktinfo.epostadresse, body.epost)
+        assertEquals(digitalKontaktinfo.mobiltelefonnummer, body.tlf)
+        assertNull(body.feilAarsak)
+    }
+
+    @Test
+    fun narmesteLeder_forbidden() {
+        loggInnBrukerTokenX(contextHolder, LEDER_FNR, oppfolgingsplanClientId, tokenxIdp)
+        `when`(brukertilgangConsumer.hasAccessToAnsatt(ARBEIDSTAKER_FNR)).thenReturn(false)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        assertEquals(403, res.statusCodeValue.toLong())
+    }
+
+    @Test
+    fun skal_ha_varsel_naar_kan_varsles_og_er_ikke_reservert() {
+        loggInnBrukerTokenX(contextHolder, LEDER_FNR, oppfolgingsplanClientId, tokenxIdp)
+
+        `when`(brukertilgangConsumer.hasAccessToAnsatt(ARBEIDSTAKER_FNR)).thenReturn(true)
+        val digitalKontaktinfo = DigitalKontaktinfo(
+            epostadresse = UserConstants.PERSON_EMAIL,
+            kanVarsles = true,
+            reservert = false,
+            mobiltelefonnummer = UserConstants.PERSON_TLF,
+            personident = ARBEIDSTAKER_FNR
+        )
+        `when`(dkifConsumer.kontaktinformasjon(ArgumentMatchers.anyString())).thenReturn(digitalKontaktinfo)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        val body = res.body as Kontaktinfo
+        assertEquals(200, res.statusCodeValue.toLong())
+        assertTrue(body.skalHaVarsel)
+    }
+
+    @Test
+    fun skal_ikke_ha_varsel_naar_ikke_kan_varsles_og_er_ikke_reservert() {
+        loggInnBrukerTokenX(contextHolder, LEDER_FNR, oppfolgingsplanClientId, tokenxIdp)
+
+        `when`(brukertilgangConsumer.hasAccessToAnsatt(ARBEIDSTAKER_FNR)).thenReturn(true)
+        val digitalKontaktinfo = DigitalKontaktinfo(
+            epostadresse = UserConstants.PERSON_EMAIL,
+            kanVarsles = false,
+            reservert = false,
+            mobiltelefonnummer = UserConstants.PERSON_TLF,
+            personident = ARBEIDSTAKER_FNR
+        )
+        `when`(dkifConsumer.kontaktinformasjon(ArgumentMatchers.anyString())).thenReturn(digitalKontaktinfo)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        val body = res.body as Kontaktinfo
+        assertEquals(200, res.statusCodeValue.toLong())
+        assertFalse(body.skalHaVarsel)
+    }
+
+    @Test
+    fun skal_ikke_ha_varsel_naar_ikke_kan_varsles_og_er_reservert() {
+        loggInnBrukerTokenX(contextHolder, LEDER_FNR, oppfolgingsplanClientId, tokenxIdp)
+
+        `when`(brukertilgangConsumer.hasAccessToAnsatt(ARBEIDSTAKER_FNR)).thenReturn(true)
+        val digitalKontaktinfo = DigitalKontaktinfo(
+            epostadresse = UserConstants.PERSON_EMAIL,
+            kanVarsles = false,
+            reservert = true,
+            mobiltelefonnummer = UserConstants.PERSON_TLF,
+            personident = ARBEIDSTAKER_FNR
+        )
+        `when`(dkifConsumer.kontaktinformasjon(ArgumentMatchers.anyString())).thenReturn(digitalKontaktinfo)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        val body = res.body as Kontaktinfo
+        assertEquals(200, res.statusCodeValue.toLong())
+        assertFalse(body.skalHaVarsel)
+    }
+
+    @Test
+    fun skal_ikke_ha_varsel_naar_kan_varsles_og_er_reservert() {
+        loggInnBrukerTokenX(contextHolder, LEDER_FNR, oppfolgingsplanClientId, tokenxIdp)
+
+        `when`(brukertilgangConsumer.hasAccessToAnsatt(ARBEIDSTAKER_FNR)).thenReturn(true)
+        val digitalKontaktinfo = DigitalKontaktinfo(
+            epostadresse = UserConstants.PERSON_EMAIL,
+            kanVarsles = true,
+            reservert = true,
+            mobiltelefonnummer = UserConstants.PERSON_TLF,
+            personident = ARBEIDSTAKER_FNR
+        )
+        `when`(dkifConsumer.kontaktinformasjon(ArgumentMatchers.anyString())).thenReturn(digitalKontaktinfo)
+
+        val res: ResponseEntity<*> = kontaktinfoController.getKontaktinfo(ARBEIDSTAKER_FNR)
+        val body = res.body as Kontaktinfo
+        assertEquals(200, res.statusCodeValue.toLong())
+        assertFalse(body.skalHaVarsel)
+    }
+
+}
