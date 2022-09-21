f2968a00baa95735b92abc2a09f1bf3b2ff2d21d
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2022-09-21 15:43:28 +0200

    TokenX-controller for Kontaktinfo (#267)

--- src/main/kotlin/no/nav/syfo/api/v3/controller/KontaktinfoControllerV3.kt
+++ src/main/kotlin/no/nav/syfo/api/v3/controller/KontaktinfoControllerV3.kt
@@ -2,74 +2,74 @@ package no.nav.syfo.api.v3.controller
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
-import no.nav.syfo.api.v2.domain.Person
+import no.nav.syfo.api.v2.domain.Kontaktinfo
 import no.nav.syfo.api.v2.util.fodselsnummerInvalid
-import no.nav.syfo.pdl.PdlConsumer
-import no.nav.syfo.pdl.fullName
+import no.nav.syfo.dkif.DigitalKontaktinfo
+import no.nav.syfo.dkif.DkifConsumer
 import no.nav.syfo.service.BrukertilgangService
 import no.nav.syfo.tokenx.TokenXUtil
 import no.nav.syfo.tokenx.TokenXUtil.TokenXIssuer.TOKENX
 import no.nav.syfo.tokenx.TokenXUtil.fnrFromIdportenTokenX
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.inject.Inject
 
+
 @RestController
 @ProtectedWithClaims(issuer = TOKENX, claimMap = ["acr=Level4"])
-@RequestMapping(value = ["/api/v3/person/{fnr}"])
-class PersonControllerV3 @Inject constructor(
+@RequestMapping(value = ["/api/v3/kontaktinfo/{fnr}"])
+class KontaktinfoControllerV3 @Inject constructor(
     private val contextHolder: TokenValidationContextHolder,
-    private val pdlConsumer: PdlConsumer,
     private val brukertilgangService: BrukertilgangService,
+    private val dkifConsumer: DkifConsumer,
     @Value("\${tokenx.idp}")
     private val tokenxIdp: String,
     @Value("\${oppfolgingsplan.frontend.client.id}")
     private val oppfolgingsplanClientId: String,
 ) {
     @ResponseBody
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getPerson(
+    @GetMapping(produces = [APPLICATION_JSON_VALUE])
+    fun getKontaktinfo(
         @PathVariable("fnr") fnr: String
-    ): ResponseEntity<Person> {
+    ): ResponseEntity<Kontaktinfo> {
         val innloggetFnr = TokenXUtil.validateTokenXClaims(contextHolder, tokenxIdp, oppfolgingsplanClientId)
             .fnrFromIdportenTokenX()
             .value
+
         return if (fodselsnummerInvalid(fnr)) {
-            LOG.error("Ugyldig fnr ved henting av person")
-            return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
+            LOG.error("Ugyldig fnr ved henting av kontaktinfo")
+            ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
                 .build()
         } else {
             if (!brukertilgangService.tilgangTilOppslattIdent(innloggetFnr, fnr)) {
-                LOG.error("Ikke tilgang til person: Bruker spør om noen andre enn seg selv eller egne ansatte")
+                LOG.error("Ikke tilgang til kontaktinfo: Bruker spør om noen andre enn seg selv eller egne ansatte")
                 ResponseEntity
                     .status(HttpStatus.FORBIDDEN)
                     .build()
             } else {
-                val personNavn = pdlConsumer.person(fnr)?.fullName()
-                personNavn?.let {
-                    ResponseEntity
-                        .status(HttpStatus.OK)
-                        .body(
-                            Person(
-                                fnr = fnr,
-                                navn = it
-                            )
-                        )
-                } ?: ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .build()
+                val kontaktinfo = dkifConsumer.kontaktinformasjon(fnr)
+                ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(Kontaktinfo(
+                        fnr = fnr,
+                        epost = kontaktinfo.epostadresse,
+                        tlf = kontaktinfo.mobiltelefonnummer,
+                        skalHaVarsel = kanVarsles(kontaktinfo)
+                    ))
             }
         }
 
     }
 
+    private fun kanVarsles(digitalKontaktinfo: DigitalKontaktinfo) =
+        (digitalKontaktinfo.reservert == false) && digitalKontaktinfo.kanVarsles
+
     companion object {
-        private val LOG: Logger = LoggerFactory.getLogger(PersonControllerV3::class.java)
+        private val LOG = LoggerFactory.getLogger(KontaktinfoControllerV3::class.java)
     }
 }
