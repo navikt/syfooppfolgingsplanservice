c290299ddb93f1cf611ceece3b60001785624688
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-09-21 15:26:21 +0200

    Add controller and test for dokument

---
+++ src/main/kotlin/no/nav/syfo/api/v2/controller/DokumentControllerV2.kt
@@ -0,0 +1,42 @@
+package no.nav.syfo.api.v2.controller
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.syfo.metric.Metrikk
+import no.nav.syfo.service.PdfService
+import no.nav.syfo.tokenx.TokenXUtil
+import no.nav.syfo.tokenx.TokenXUtil.TokenXIssuer.TOKENX
+import no.nav.syfo.tokenx.TokenXUtil.fnrFromIdportenTokenX
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.inject.Inject
+
+@RestController
+@ProtectedWithClaims(issuer = TOKENX, claimMap = ["acr=Level4"])
+@RequestMapping(value = ["/api/v2/dokument/{oppfolgingsplanId}/ekstern"])
+class DokumentControllerV2 @Inject constructor(
+    private val contextHolder: TokenValidationContextHolder,
+    private val pdfService: PdfService,
+    private val metrikk: Metrikk,
+    @Value("\${tokenx.idp}")
+    private val tokenxIdp: String,
+    @Value("\${oppfolgingsplan.frontend.client.id}")
+    private val oppfolgingsplanClientId: String,
+) {
+    @GetMapping
+    fun hentPdf(@PathVariable("oppfolgingsplanId") oppfolgingsplanId: Long): ResponseEntity<*> {
+        val innloggetIdent = TokenXUtil.validateTokenXClaims(contextHolder, tokenxIdp, oppfolgingsplanClientId)
+            .fnrFromIdportenTokenX()
+            .value
+        val pdf = pdfService.hentPdf(oppfolgingsplanId, innloggetIdent)
+        metrikk.tellHendelse("hent_pdf")
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(pdf)
+    }
+}
