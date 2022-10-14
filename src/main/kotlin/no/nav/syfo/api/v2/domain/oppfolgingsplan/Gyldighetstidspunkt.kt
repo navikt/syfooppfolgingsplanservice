6216fe8c7871d84827a9230e40a42d9b0a096fd4
AudunSorheim <audun.sorheim@nav.no>
2022-10-14 11:58:28 +0200

    Add a few optional types

--- src/main/kotlin/no/nav/syfo/api/v2/domain/oppfolgingsplan/Gyldighetstidspunkt.kt
+++ src/main/kotlin/no/nav/syfo/api/v2/domain/oppfolgingsplan/Gyldighetstidspunkt.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.api.v2.domain.oppfolgingsplan
 import java.time.LocalDate
 
 data class Gyldighetstidspunkt(
-    val fom: LocalDate,
-    val tom: LocalDate,
-    val evalueres: LocalDate,
+    val fom: LocalDate?,
+    val tom: LocalDate?,
+    val evalueres: LocalDate
 )
