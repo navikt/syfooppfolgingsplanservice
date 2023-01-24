e8dbbc515145feab7d87ced7a62db04598e1fbfa
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-01-24 13:35:50 +0100

    Remove AktorregisterConsumer

--- src/main/java/no/nav/syfo/aktorregister/exceptions/NoCurrentIdentForAktor.java
+++
@@ -1,7 +0,0 @@
-package no.nav.syfo.aktorregister.exceptions;
-
-public class NoCurrentIdentForAktor extends RuntimeException {
-    public NoCurrentIdentForAktor(String message) {
-        super(message);
-    }
-}
--- src/main/java/no/nav/syfo/aktorregister/exceptions/NoResponseForDesiredUser.java
+++
@@ -1,7 +0,0 @@
-package no.nav.syfo.aktorregister.exceptions;
-
-public class NoResponseForDesiredUser extends RuntimeException{
-    public NoResponseForDesiredUser(String message) {
-        super(message);
-    }
-}
