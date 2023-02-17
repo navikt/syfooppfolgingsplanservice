ebdf17fced7021cd6cd39b9ca4437a051049037f
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2023-02-17 12:11:45 +0100

    Fix cache for Felles kodeverk (#309)

--- src/main/java/no/nav/syfo/fellesKodeverk/KodeverkKoderBetydningerResponse.java
+++ src/main/java/no/nav/syfo/fellesKodeverk/KodeverkKoderBetydningerResponse.java
@@ -3,11 +3,12 @@ package no.nav.syfo.fellesKodeverk;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
 @Data
 @Accessors(fluent = true)
-public class KodeverkKoderBetydningerResponse {
+public class KodeverkKoderBetydningerResponse implements Serializable {
     public Map<String, List<Betydning>> betydninger;
 }
--- src/test/kotlin/no/nav/syfo/fellesKodeverk/FellesKodeverkConsumerTest.kt
+++ src/test/kotlin/no/nav/syfo/fellesKodeverk/FellesKodeverkConsumerTest.kt
@@ -24,26 +24,8 @@ class FellesKodeverkConsumerTest : TestCase() {
     private lateinit var fellesKodeverkConsumer: FellesKodeverkConsumer
 
     @Test
-    fun stillingsnavnFromKode_gives_correct_stillingsnavn() {
-        val expectedResponse = responseBody()
-        Mockito.`when`(restTemplate.exchange(ArgumentMatchers.anyString(), ArgumentMatchers.eq(HttpMethod.GET), ArgumentMatchers.any(HttpEntity::class.java), ArgumentMatchers.eq(KodeverkKoderBetydningerResponse::class.java))).thenReturn(ResponseEntity(expectedResponse, HttpStatus.OK))
-        val actualStillingsnavn = fellesKodeverkConsumer.stillingsnavnFromKode(STILLINGSKODE)
-        Assertions.assertThat(actualStillingsnavn).isEqualTo(STILLINGSNAVN_LOWERCAPITALIZED)
-        Mockito.verify(metric).tellHendelse("call_felleskodeverk_success")
-    }
-
-    @Test
-    fun stillingsnavnFromKode_return_custom_message_if_navn_not_found() {
-        val expectedResponse = responseBodyWithWrongKode()
-        Mockito.`when`(restTemplate.exchange(ArgumentMatchers.anyString(), ArgumentMatchers.eq(HttpMethod.GET), ArgumentMatchers.any(HttpEntity::class.java), ArgumentMatchers.eq(KodeverkKoderBetydningerResponse::class.java))).thenReturn(ResponseEntity(expectedResponse, HttpStatus.OK))
-        val actualStillingsnavn = fellesKodeverkConsumer.stillingsnavnFromKode(STILLINGSKODE)
-        Assertions.assertThat(actualStillingsnavn).isEqualTo("Ugyldig yrkeskode $STILLINGSKODE")
-        Mockito.verify(metric).tellHendelse("call_felleskodeverk_success")
-    }
-
-    @Test
     fun get_kodeverkKoderBetydninger() {
-        val expectedResponse = responseBody()
+        val expectedResponse = fellesKodeverkResponseBody(STILLINGSNAVN, STILLINGSKODE)
         Mockito.`when`(restTemplate.exchange(ArgumentMatchers.anyString(), ArgumentMatchers.eq(HttpMethod.GET), ArgumentMatchers.any(HttpEntity::class.java), ArgumentMatchers.eq(KodeverkKoderBetydningerResponse::class.java))).thenReturn(ResponseEntity(expectedResponse, HttpStatus.OK))
         val actualResponse = fellesKodeverkConsumer.kodeverkKoderBetydninger()
         Assertions.assertThat(actualResponse.betydninger[STILLINGSKODE]).isNotNull
@@ -62,46 +44,5 @@ class FellesKodeverkConsumerTest : TestCase() {
             Assertions.assertThat(e.javaClass).isEqualTo(RuntimeException::class.java)
             Assertions.assertThat(e.message).isEqualTo("Tried to get kodeBetydninger from Felles Kodeverk")
         }
-    }
-
-    private fun responseBody(): KodeverkKoderBetydningerResponse {
-        val beskrivelse = Beskrivelse()
-            .tekst(STILLINGSNAVN)
-            .term(STILLINGSNAVN)
-        val beskrivelser: MutableMap<String, Beskrivelse> = HashMap()
-        beskrivelser[SPRAK] = beskrivelse
-        val betydning = Betydning()
-            .beskrivelser(beskrivelser)
-            .gyldigFra(Date().toString())
-            .gyldigTil(Date().toString())
-        val betydninger: MutableMap<String, List<Betydning>> = HashMap()
-        betydninger[STILLINGSKODE] = listOf(betydning)
-        return KodeverkKoderBetydningerResponse()
-            .betydninger(betydninger)
-    }
-
-    private fun responseBodyWithWrongKode(): KodeverkKoderBetydningerResponse {
-        val beskrivelse = Beskrivelse()
-            .tekst(WRONG_STILLINGSNAVN)
-            .term(WRONG_STILLINGSNAVN)
-        val beskrivelser: MutableMap<String, Beskrivelse> = HashMap()
-        beskrivelser[SPRAK] = beskrivelse
-        val betydning = Betydning()
-            .beskrivelser(beskrivelser)
-            .gyldigFra(Date().toString())
-            .gyldigTil(Date().toString())
-        val betydninger: MutableMap<String, List<Betydning>> = HashMap()
-        betydninger[WRONG_STILLINGSKODE] = listOf(betydning)
-        return KodeverkKoderBetydningerResponse()
-            .betydninger(betydninger)
-    }
-
-    companion object {
-        private const val STILLINGSNAVN = "Special Agent"
-        private const val STILLINGSNAVN_LOWERCAPITALIZED = "Special agent"
-        private const val WRONG_STILLINGSNAVN = "Deputy Director"
-        private const val STILLINGSKODE = "1234567"
-        private const val WRONG_STILLINGSKODE = "9876543"
-        private const val SPRAK = "nb"
     }
 }
