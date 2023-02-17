e6aa854399718dd071498dcd88a7f095c7420710
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-02-17 11:39:02 +0100

    Make serializable

--- src/main/java/no/nav/syfo/fellesKodeverk/Beskrivelse.java
+++ src/main/java/no/nav/syfo/fellesKodeverk/Beskrivelse.java
@@ -3,9 +3,11 @@ package no.nav.syfo.fellesKodeverk;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 @Data
 @Accessors(fluent = true)
-public class Beskrivelse {
+public class Beskrivelse implements Serializable {
     public String tekst;
     public String term;
 }
