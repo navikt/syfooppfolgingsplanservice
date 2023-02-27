40c2b4630ed8903d483787aba14ee7b4515f581d
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-02-27 15:22:06 +0100

    Remove unused KontaktinfoControllerV2

--- src/test/kotlin/no/nav/syfo/api/v3/controller/KontaktinfoControllerV3Test.kt
+++ src/test/kotlin/no/nav/syfo/api/v3/controller/KontaktinfoControllerV3Test.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo.api.v3.controller
 
 import no.nav.syfo.api.AbstractRessursTilgangTest
-import no.nav.syfo.api.v2.domain.Kontaktinfo
+import no.nav.syfo.api.v3.domain.Kontaktinfo
 import no.nav.syfo.brukertilgang.BrukertilgangConsumer
 import no.nav.syfo.dkif.DigitalKontaktinfo
 import no.nav.syfo.dkif.DkifConsumer
