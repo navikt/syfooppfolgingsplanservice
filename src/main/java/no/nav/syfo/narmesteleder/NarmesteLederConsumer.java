50c9d111958cf7e4ecca05813e735799b9bac53d
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2023-11-28 08:07:27 +0100

    Fix SSRF vulnerability (#399)

--- src/main/java/no/nav/syfo/narmesteleder/NarmesteLederConsumer.java
+++ src/main/java/no/nav/syfo/narmesteleder/NarmesteLederConsumer.java
@@ -11,13 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static java.util.stream.Collectors.toList;
 import static no.nav.syfo.config.CacheConfig.CACHENAME_ANSATTE;
 import static no.nav.syfo.config.CacheConfig.CACHENAME_LEDER;
 import static no.nav.syfo.util.CredentialUtilKt.bearerHeader;
@@ -99,10 +97,11 @@ public class NarmesteLederConsumer {
         String token = azureAdTokenConsumer.getAccessToken(narmestelederScope);
 
         ResponseEntity<NarmestelederResponse> response = restTemplate.exchange(
-                getLederUrl(virksomhetsnummer),
+                getLederUrl(),
                 GET,
                 entityForSykmeldt(token, fnr),
-                NarmestelederResponse.class
+                NarmestelederResponse.class,
+                virksomhetsnummer
         );
         throwExceptionIfError(response.getStatusCode(), HENT_LEDER_NARMESTELEDER_FEILET);
 
@@ -146,14 +145,14 @@ public class NarmesteLederConsumer {
         return narmestelederUrl + "/leder/narmesteleder/aktive";
     }
 
-    private String getLederUrl(String virksomhetsnummer) {
-        return UriComponentsBuilder.fromHttpUrl(narmestelederUrl + "/sykmeldt/narmesteleder").queryParam("orgnummer", virksomhetsnummer).toUriString();
+    private String getLederUrl() {
+        return narmestelederUrl + "/sykmeldt/narmesteleder?orgnummer={virksomhetsnummer}";
     }
 
     public boolean erNaermesteLederForAnsatt(String naermesteLederFnr, String ansattFnr) {
         List<String> ansatteFnr = ansatte(naermesteLederFnr).stream()
                 .map(Ansatt::fnr)
-                .collect(toList());
+                .toList();
 
         return ansatteFnr.contains(ansattFnr);
     }
