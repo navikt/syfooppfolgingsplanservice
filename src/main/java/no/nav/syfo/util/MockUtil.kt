acf745fedd07a1ac8432f8938ce4f49605cfaf16
AudunSorheim <audun.sorheim@nav.no>
2024-02-21 15:26:49 +0100

    Add visma test org

--- src/main/java/no/nav/syfo/util/MockUtil.kt
+++ src/main/java/no/nav/syfo/util/MockUtil.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.util
 private const val defaultOrgNumber = "910067494"
 private const val vismaTestUserOrgNumberInNAV = "972674818"
 private const val vismaTestUserOrgNumberInAltinn = "215918912"
-private val allowedOrgnumbersToBeSentToAltinn = listOf(defaultOrgNumber)
+private val allowedOrgnumbersToBeSentToAltinn = listOf(defaultOrgNumber, vismaTestUserOrgNumberInAltinn)
 
 fun getOrgnummerForSendingTilAltinn(orgnumber: String): String {
     if (!isDev()) return orgnumber
