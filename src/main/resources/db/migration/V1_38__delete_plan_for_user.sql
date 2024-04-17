30cfe6114dc7122c6c99f4fc1e86dabdb80f94c7
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2024-04-17 09:47:47 +0200

    Delete old plan (#449)

---
+++ src/main/resources/db/migration/V1_38__delete_plan_for_user.sql
@@ -0,0 +1,3 @@
+delete from arbeidsoppgave where oppfoelgingsdialog_id = '493449';
+delete from tiltak where oppfoelgingsdialog_id = '493449';
+delete from oppfoelgingsdialog where oppfoelgingsdialog_id = '493449';
