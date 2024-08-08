bb26df1d3a7b3143a42cb7603c14ec384ce906d5
Malaz Alkoj <malaz.alkoj@nav.no>
2024-08-08 15:50:28 +0200

    Update lib for better security. (#478)
    * Update lib for better security.
    
    * Minor update to avoid test failures and some unused libs included in the newer versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ version = "1.0.0"
 object Versions {
     const val avroVersion = "1.11.0"
     const val confluentVersion = "7.1.0"
-    const val cxfVersion = "3.6.2"
+    const val cxfVersion = "3.6.4"
     const val flywayVersion = "9.10.0"
     const val tokenSupportVersion = "3.2.0"
     const val tokenTestSupportVersion = "2.0.5"
