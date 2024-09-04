eabadecc0295f24b1bd80e661931e262e9b0ccc4
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-09-04 09:21:38 +0200

    Kodeverk azure (#480)

--- src/main/java/no/nav/syfo/fellesKodeverk/FellesKodeverkConsumer.java
+++ src/main/java/no/nav/syfo/fellesKodeverk/FellesKodeverkConsumer.java
@@ -1,19 +1,22 @@
 package no.nav.syfo.fellesKodeverk;
 
+import no.nav.syfo.azuread.AzureAdTokenConsumer;
 import no.nav.syfo.metric.Metrikk;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import static no.nav.syfo.config.CacheConfig.CACHENAME_FELLESKODEVERK_BETYDNINGER;
+import static no.nav.syfo.util.CredentialUtilKt.bearerHeader;
 import static no.nav.syfo.util.RequestUtilKt.APP_CONSUMER_ID;
 import static no.nav.syfo.util.RequestUtilKt.createCallId;
-import static no.nav.syfo.util.StringUtilKt.lowerCapitalize;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpMethod.GET;
 
@@ -23,7 +26,10 @@ public class FellesKodeverkConsumer {
 
     private final Metrikk metric;
     private final RestTemplate restTemplateMedProxy;
-    private final String url;
+    private final String felleskodeverkUrl;
+    private final String felleskodeverkScope;
+
+    private final AzureAdTokenConsumer azureAdTokenConsumer;
 
     public static final String NAV_CALL_ID_HEADER = "Nav-Call-Id";
     public static final String NAV_CONSUMER_ID = "Nav-Consumer-Id";
@@ -32,25 +38,32 @@ public class FellesKodeverkConsumer {
     public FellesKodeverkConsumer(
             Metrikk metric,
             RestTemplate restTemplateMedProxy,
-            @Value("${felleskodeverk.url}") String url
+            @Value("${felleskodeverk.url}") String felleskodeverkUrl,
+            @Value("${felleskodeverk.scope}") String felleskodeverkScope,
+            AzureAdTokenConsumer azureAdTokenConsumer
     ) {
         this.metric = metric;
         this.restTemplateMedProxy = restTemplateMedProxy;
-        this.url = url;
+        this.felleskodeverkUrl = felleskodeverkUrl;
+        this.felleskodeverkScope = felleskodeverkScope;
+        this.azureAdTokenConsumer = azureAdTokenConsumer;
     }
 
     @Cacheable(cacheNames = CACHENAME_FELLESKODEVERK_BETYDNINGER)
     public KodeverkKoderBetydningerResponse kodeverkKoderBetydninger() {
-        String kodeverkYrkerBetydningUrl = url + "/kodeverk/Yrker/koder/betydninger?spraak=nb";
+        String kodeverkYrkerBetydningUrl = felleskodeverkUrl + "/kodeverk/Yrker/koder/betydninger?spraak=nb";
+
+        String accessToken = azureAdTokenConsumer.getAccessToken(felleskodeverkScope);
 
         try {
             ResponseEntity<KodeverkKoderBetydningerResponse> response = restTemplateMedProxy.exchange(
                     kodeverkYrkerBetydningUrl,
                     GET,
-                    entity(),
+                    entity(accessToken),
                     KodeverkKoderBetydningerResponse.class
             );
             metric.tellHendelse("call_felleskodeverk_success");
+            LOG.info("Successfully fetched KodeverkKoderBetydninger from felleskodeverk");
             return response.getBody();
         } catch (RestClientException e) {
             metric.tellHendelse("call_felleskodeverk_fail");
@@ -59,23 +72,9 @@ public class FellesKodeverkConsumer {
         }
     }
 
-    public String stillingsnavnFromKode(String stillingskode) {
-        KodeverkKoderBetydningerResponse response = kodeverkKoderBetydninger();
-        try {
-            String stillingsnavn = stillingsnavnFromKodeverkKoderBetydningerResponse(response, stillingskode);
-            return lowerCapitalize(stillingsnavn);
-        } catch (NullPointerException e) {
-            LOG.error("Couldn't find navn for stillingskode: " + stillingskode);
-            return "Ugyldig yrkeskode " + stillingskode;
-        }
-    }
-
-    private String stillingsnavnFromKodeverkKoderBetydningerResponse(KodeverkKoderBetydningerResponse response, String stillingskode) {
-        return response.betydninger.get(stillingskode).get(0).beskrivelser.get("nb").tekst;
-    }
-
-    private HttpEntity entity() {
+    private HttpEntity entity(String token) {
         HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, bearerHeader(token));
         headers.add(NAV_CALL_ID_HEADER, createCallId());
         headers.add(NAV_CONSUMER_ID, APP_CONSUMER_ID);
         return new HttpEntity<>(headers);
