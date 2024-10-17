e7393100355d908082f113a697f2f1890b40d816
malazalkoj <malaz.alkoj@nav.no>
2024-10-17 16:13:24 +0200

    Delete old plan after request from veileder

---
+++ src/main/resources/db/migration/V1_39__delete_plan.sql
@@ -0,0 +1,3 @@
+delete from tiltak where oppfoelgingsdialog_id = '60523';
+delete from arbeidsoppgave where oppfoelgingsdialog_id = '60523';
+delete from oppfoelgingsdialog where oppfoelgingsdialog_id = '60523';
