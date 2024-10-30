5c4629444f02f3475947b351e0f1f8483286eeb4
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-10-30 15:03:51 +0100

    Add delete from godkjentplan (#482)
    * Add delete from godkjentplan
    
    * Fix query

---
+++ src/main/resources/db/migration/V1_40__delete_plan.sql
@@ -0,0 +1,4 @@
+delete from godkjentplan where oppfoelgingsdialog_id = '60523';
+delete from tiltak where oppfoelgingsdialog_id = '60523';
+delete from arbeidsoppgave where oppfoelgingsdialog_id = '60523';
+delete from oppfoelgingsdialog where oppfoelgingsdialog_id = '60523';
