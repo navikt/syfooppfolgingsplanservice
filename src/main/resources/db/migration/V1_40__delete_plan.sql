79ff53741a29575d45460b47747644316fe4713a
Tanja Krasnomovets <victorandreevi4@gmail.com>
2024-10-30 14:48:11 +0100

    Add delete from godkjentplan

---
+++ src/main/resources/db/migration/V1_40__delete_plan.sql
@@ -0,0 +1,4 @@
+delete from tiltak where oppfoelgingsdialog_id = '60523';
+delete from arbeidsoppgave where oppfoelgingsdialog_id = '60523';
+select * from godkjentplan where oppfoelgingsdialog_id = '60523';
+delete from oppfoelgingsdialog where oppfoelgingsdialog_id = '60523';
