da609641e044698e094347c0ef697b32855d9367
malazalkoj <malaz.alkoj@nav.no>
2024-11-26 13:36:37 +0100

    Try without updating ojdbcversion

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ object Versions {
     const val flywayVersion = "9.10.0"
     const val tokenSupportVersion = "3.2.0"
     const val tokenTestSupportVersion = "2.0.5"
-    const val ojdbc8Version = "21.9.0.0"
+    const val ojdbc8Version = "19.3.0.0"
     const val helseXmlVersion = "1.0.4"
     const val syfotjenesterVersion = "1.2020.07.02-07.44-62078cd74f7e"
     const val tjenesteSpesifikasjonerVersion = "1.2020.06.23-15.31-57b909d0a05c"
@@ -135,7 +135,7 @@ dependencies {
     }
 
     implementation("org.flywaydb:flyway-core:${Versions.flywayVersion}")
-    implementation("com.oracle.ojdbc:ojdbc11:${Versions.ojdbc8Version}")
+    implementation("com.oracle.ojdbc:ojdbc8:${Versions.ojdbc8Version}")
 
     implementation("io.micrometer:micrometer-registry-prometheus:1.13.2")
     implementation("net.logstash.logback:logstash-logback-encoder:4.10")
