ac09ea47728045acc842a71e12ba2deb28b50f3e
malazkoj <malaz.alkoj@nav.no>
2025-05-06 09:51:41 +0200

    Add godkjentplan_view with check to not fail if view is already exists.

--- src/main/resources/db/migration/V1_41__Add_dhv_read_user.sql
+++ src/main/resources/db/migration/V1_41__Add_dhv_read_user.sql
@@ -1,5 +1,16 @@
-CREATE VIEW GODKJENTPLAN_VIEW AS SELECT SM_FNR, VIRKSOMHETSNUMMER, G.CREATED FROM oppfoelgingsdialog o INNER JOIN godkjentplan g ON o.oppfoelgingsdialog_id= g.oppfoelgingsdialog_id
-    AND g.delt_med_nav=1;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.views WHERE table_name = 'godkjentplan_view') THEN
+CREATE VIEW GODKJENTPLAN_VIEW AS
+SELECT SM_FNR, VIRKSOMHETSNUMMER, G.CREATED
+FROM oppfoelgingsdialog o
+         INNER JOIN godkjentplan g
+                    ON o.oppfoelgingsdialog_id = g.oppfoelgingsdialog_id
+                        AND g.delt_med_nav = 1;
+END IF;
+END
+$$;
+
 DO $$
 BEGIN
         CREATE USER "dvh_les";
