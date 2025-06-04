f26b61ae734572c5d04b60bb445db8f79d5e1450
malazkoj <malaz.alkoj@nav.no>
2025-06-04 14:59:26 +0200

    Only create view. The user is created by the DBA team.

--- src/main/resources/db/migration/V1_41__Add_dhv_read_user.sql
+++ src/main/resources/db/migration/V1_41__Add_dhv_read_user.sql
@@ -1,34 +1,7 @@
-DECLARE
-view_exists NUMBER;
-BEGIN
-SELECT COUNT(*)
-INTO view_exists
-FROM all_views
-WHERE view_name = 'GODKJENTPLAN_VIEW';
-
-IF view_exists = 0 THEN
-        EXECUTE IMMEDIATE 'CREATE VIEW GODKJENTPLAN_VIEW AS
-                           SELECT SM_FNR, VIRKSOMHETSNUMMER, G.CREATED
+CREATE OR REPLACE FORCE EDITIONABLE VIEW GODKJENTPLAN_VIEW AS
+                           SELECT SM_FNR, VIRKSOMHETSNUMMER, O.CREATED AS oppfoelgingsdialog_created,
+                                  G.CREATED AS GODKJENTPLAN_CREATED, G.DELT_MED_NAV_TIDSPUNKT
                            FROM oppfoelgingsdialog o
                            INNER JOIN godkjentplan g
                            ON o.oppfoelgingsdialog_id = g.oppfoelgingsdialog_id
-                           AND g.delt_med_nav = 1';
-END IF;
-END;
-/
-
-DECLARE
-user_exists NUMBER;
-BEGIN
-SELECT COUNT(*)
-INTO user_exists
-FROM all_users
-WHERE username = 'DVH_LES' OR username = 'dvh_les';
-
-IF user_exists = 0 THEN
-        EXECUTE IMMEDIATE 'CREATE USER dvh_les';
-END IF;
-END;
-/
-
-GRANT SELECT ON GODKJENTPLAN_VIEW TO dvh_les;
+                           AND g.delt_med_nav = 1;
