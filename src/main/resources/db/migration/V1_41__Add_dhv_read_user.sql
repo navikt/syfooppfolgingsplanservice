8af4867ef8a8ba32f5e003a39310b39bcfe414ad
malazkoj <malaz.alkoj@nav.no>
2025-06-05 11:22:37 +0200

    Autoformat

--- src/main/resources/db/migration/V1_41__Add_dhv_read_user.sql
+++ src/main/resources/db/migration/V1_41__Add_dhv_read_user.sql
@@ -1,7 +1,11 @@
-CREATE OR REPLACE FORCE EDITIONABLE VIEW GODKJENTPLAN_VIEW AS
-                           SELECT SM_FNR, VIRKSOMHETSNUMMER, O.CREATED AS oppfoelgingsdialog_created,
-                                  G.CREATED AS GODKJENTPLAN_CREATED, G.DELT_MED_NAV_TIDSPUNKT
-                           FROM oppfoelgingsdialog o
-                           INNER JOIN godkjentplan g
-                           ON o.oppfoelgingsdialog_id = g.oppfoelgingsdialog_id
-                           AND g.delt_med_nav = 1;
+CREATE
+OR REPLACE FORCE EDITIONABLE VIEW GODKJENTPLAN_VIEW AS
+SELECT SM_FNR,
+       VIRKSOMHETSNUMMER,
+       O.CREATED AS oppfoelgingsdialog_created,
+       G.CREATED AS GODKJENTPLAN_CREATED,
+       G.DELT_MED_NAV_TIDSPUNKT
+FROM oppfoelgingsdialog o
+         INNER JOIN godkjentplan g
+                    ON o.oppfoelgingsdialog_id = g.oppfoelgingsdialog_id
+                        AND g.delt_med_nav = 1;
